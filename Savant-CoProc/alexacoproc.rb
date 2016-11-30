43b818a537ef345ac1a5da8f25ae5509486551c2
caboose014 <j_14@me.com>
2016-12-01 10:31:00 +1300

    Building XML parser

--- Savant-CoProc/alexacoproc.rb
+++ Savant-CoProc/alexacoproc.rb
@@ -3,6 +3,7 @@
 require 'socket'
 require 'net/http'
 require 'rexml/document'
+include REXML
 
 # Setup some environment variables to start with
 scli = "~/Applications/RacePointMedia/sclibridge "
@@ -19,4 +20,8 @@ end
 # Lets get the services xml file loaded
 servicesdoc = Document.new(File.new(configxml))
 
-#
+# Start building the zones and resources
+servicesdoc.each_element("//zone") do |zone|
+    next if zone.attributes["type"] == "resource"
+    puts zone.attributes["name"]
+end
