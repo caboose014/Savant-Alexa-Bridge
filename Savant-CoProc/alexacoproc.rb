04a5b84e72fd06491dda5b9156a77a738e56f178
caboose014 <j_14@me.com>
2016-12-01 13:40:08 +1300

    Building xml parser

--- Savant-CoProc/alexacoproc.rb
+++ Savant-CoProc/alexacoproc.rb
@@ -9,6 +9,7 @@ include REXML
 scli = "~/Applications/RacePointMedia/sclibridge "
 servicefile = "userConfig.rpmConfig/serviceImplementation.xml"
 configxml = "/Users/RPM/Library/Application Support/RacePointMedia/" + servicefile
+liveservices = {}
 
 # Check to see if we are running on a linux host, if so we need to change the variables
 platform = RUBY_PLATFORM
@@ -25,6 +26,18 @@ servicesdoc = Document.new(File.new(configxml))
 
 # Start building the zones and resources
 servicesdoc.each_element("//zone") do |zone|
-    next if zone.attributes["type"] == "resource"
+    # Skip if this is not a user zone
+    next if zone.attributes["type"] != "user"
+    puts "+==================="
     puts zone.attributes["name"]
+    liveservices[zone.attributes["name"]] = {}
+
+    zone.each_element("service") do |service|
+        next unless service.attributes["service_type"].start_with?("SVC_AV_", "SVC_ENV_") && service.attributes["enabled"] == "true"
+        next if service.attributes["service_type"] == "SVC_ENV_AV_DOORBELL"
+
+        puts service.attributes["service_alias"]
+    end
 end
+
+puts liveservices
